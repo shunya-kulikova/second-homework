82cf5dadf8e33a1636a1bbe96a924257f5e87004
unknown <a.kulikova.front@gmail.com>
2022-04-06 23:41:28 +0300

    third homework done

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let firstNumber = Number(prompt('Enter the first number'));
+let mathOperation = prompt('Enter a math operation (-, +, /, *)');
+let secondNumber = Number(prompt('Enter the second number'));
+let result;
+
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert('This is not a number!')
+}
+
+
+if (mathOperation === '-') {
+    result = firstNumber - secondNumber;
+
+} else if (mathOperation === '+') {
+    result = firstNumber + secondNumber;
+
+} else if (mathOperation === '*') {
+    result = firstNumber * secondNumber;
+
+} else if (mathOperation === '/') {
+    result = firstNumber / secondNumber;
+    
+} else {
+    alert('This math operation does not exist!')
+    
+}
+
+
+let alertMessage = `Your result is ${result}`;
+alert(alertMessage)
